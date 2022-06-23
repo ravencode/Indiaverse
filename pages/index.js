0f49a77d9a587968b94aa421e6f30c8f4a1bd4dd
Bharat Kharbanda <bharatkharbanda4@gmail.com>
2022-06-24 03:52:31 +0530

    goodnight

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
   return (
     <div className="h-screen">
       <Head>
-        <title>KrifVerse</title>
+        <title>IndiaVerse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <VideoPlayer
@@ -30,12 +30,12 @@ export default function Home() {
           "https://cdn-cf-east.streamable.com/video/mp4/k2ej0k.mp4?Expires=1656280920&Signature=V5jnPdQu~LDZxD-a1ANK0som6V9aPIkV8dfArDTibqxW8SoEAqAoxnCXD1Jb2i3B9fFddWLWeT6D5c5AL4v-Li4uqAvMNFMBdFKBto48xgbBWl5wiPj55tEf4pX5z6HmqYxd8FWlweepcBI0PKfIdHMhDBYN9cm1xuDWsarHiwi-2cWdEhRLzxoQXRrw09Q3sjz9iQ2vVOFJ0AEFtE6Qtb5kezI5haKdUyWak7zF0zLCnqj1RyxCq7Vzwa6QHxte9mJmTmAzLziEe99KmD89A9iOO-0Gg~hCqwLCy23ksoMvWQsA-sL-SDsLr63QdUWMn~hhcPnirlvrki2vhOk7OA__&Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ"
         }
         autoPlay={true}
-        muted={true}
+        muted={false}
       />
-      <div className='flex flex-col absolute z-50 h-4/6 w-full items-center justify-center
+      <div className='flex absolute z-50 w-full right-0
         space-y-4'>
-        <h1>Welcome to the App</h1>
-        <button onClick={logout}>Logout</button>
+        {/* <h1>Welcome to the App</h1> */}
+        <button className="font-bold text-white"onClick={logout}>Logout</button>
       </div>
       
     </div>
